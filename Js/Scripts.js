16bc46c451e8bfc331abbd6a9a6d2a5d7b6f5f88
Mohd Rejoan <wearecrx@gmail.com>
2021-03-14 18:52:17 +0600

    fix responsive issue (bugs)

--- Js/Scripts.js
+++ Js/Scripts.js
@@ -3,6 +3,8 @@
 // -------------------------------------------------------------------------
 const btn = document.querySelector('#nav_btn i');
 const getBtn = btn.classList;
+// get menu 
+const menu = document.querySelector("#nav_wrapper");
 console.log(getBtn)
 
 // chnage icons 
@@ -10,11 +12,25 @@ function changeIcon() {
     if (getBtn.contains('fa-bars')) {
         getBtn.remove('fa-bars');
         getBtn.add('fa-times');
+        menu.style.opacity = 1;
+        menu.style.width = "100%";
+        setTimeout(() => {
+            menu.style.display = 'block';
+        }, 400);
     } else {
         getBtn.remove('fa-times');
         getBtn.add('fa-bars')
+        menu.style.opacity = 0;
+        menu.style.width = "50%";
+        setTimeout(() => {
+            menu.style.display = "none";
+        }, 400);
     }
 }
 
+
+
 // fire ad remove function 
-btn.addEventListener('click', changeIcon);
+btn.addEventListener('click', () => {
+    changeIcon();
+});
